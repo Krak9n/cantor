1de0b51f827fac2c41d28a27b8cf018738d95a3f
louis <sanicisspeedandfast@gmail.com>
2026-07-30 21:29:18 -0400

    small changes

--- src/bar.rs
+++ src/bar.rs
@@ -1,10 +1,10 @@
 const CONFIG_PATH: &str = "~/.config/cantor/config.toml";
 const ALTERNATIVE_CONFIG_PATH: &str = "example.toml";
 
-use std::path::{Path, PathBuf};
-use std::fs;
 use serde::Deserialize;
 use std::collections::HashMap;
+use std::fs;
+use std::path::{Path, PathBuf};
 // serde here is used to deserialize the toml file into
 // a struct. goddamit why caant they read from a txt file :((()))
 
@@ -61,19 +61,19 @@ struct ModuleSettings {
 struct WorkspacesSettings {
     position: ModulePosition,
     color: String,
-    icons: HashMap<usize, String>,  // my icons
+    icons: HashMap<usize, String>, // my icons
 }
 
 pub fn read_the_config() -> Config {
     let mut config: PathBuf = PathBuf::new();
-    let configs = [ Path::new(&CONFIG_PATH), Path::new(&ALTERNATIVE_CONFIG_PATH) ];
+    let configs = [Path::new(&CONFIG_PATH), Path::new(&ALTERNATIVE_CONFIG_PATH)];
     // bitch ass search
     match configs.iter().find(|config| config.exists()) {
         Some(found) => {
             println!("Using config file under: {:?}", found);
             config = (&(**found)).to_path_buf();
-        },
-        None => panic!("None of the required config files exists. Please, create a config file!")
+        }
+        None => panic!("None of the required config files exists. Please, create a config file!"),
     }
     let contents = fs::read_to_string(&config)
         .unwrap_or_else(|e| panic!("Failed to read the config {:?} contents: {}", config, e));
